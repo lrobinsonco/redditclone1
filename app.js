781dc47cf84392a8f818e2cc8aaad3db9ab9dc76
lrobinsonco <lrobinsonco@gmail.com>
2017-04-18 10:30:12 -0600

    deploy heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const linkQuery = require('./db/link-query');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 
