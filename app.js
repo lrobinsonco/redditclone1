2e609a60802b54c08ffb7a0c378b3ca23ac5a31b
lrobinsonco <lrobinsonco@gmail.com>
2017-04-18 15:33:49 -0600

    fixed seed

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// bcrypt
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+// bcrypt saltRounds
+bcrypt.genSalt(saltRounds).then(salt =>{
+  console.log(salt);
+  bcrypt.hash(myPlaintextPassword, salt).then(hash => {
+    console.log(hash);
+  });
+});
+
+// bcrypt.compare('not_bacon', kjfhdsjhf).then(isTrue => console.log(isTrue));
+
 app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
--- seeds/01_links.js
+++ seeds/01_links.js
@@ -5,11 +5,13 @@ exports.seed = function(knex, Promise) {
             // Inserts seed entries
             return knex('link').insert([
               {
+                    id: 1,
                     votes: 35,
                     url: 'https://www.google.com',
                     title: 'hello google'
                 },
                 {
+                    id: 2,
                     votes: 40,
                     url: 'https://www.google.com',
                     title: 'hi google'
