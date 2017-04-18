a5f253b5f668f3eb63394447e3bdd6f43a28f81f
lrobinsonco <lrobinsonco@gmail.com>
2017-04-18 11:09:10 -0600

    deployed

--- db/knex.js
+++ db/knex.js
@@ -1,6 +1,6 @@
 const config = require("../knexfile");
 
-const environment = 'development';
+const environment = process.env.NODE_ENV || 'development';
 
 const pg = require('knex')(config[environment]);
 
--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,15 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/reddit'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + "?ssl=true"
   }
 
 };
